442cf9c1e2c161f1abf53645d1c3c7d906fc305b
Piet Snot <pietmuis@gmail.com>
2017-12-11 23:26:08 +0100

    AdventOfCode2017 kijken of dat wil lukken

--- src/adventofcode2017/Exercise11.java
+++ src/adventofcode2017/Exercise11.java
@@ -13,7 +13,6 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
